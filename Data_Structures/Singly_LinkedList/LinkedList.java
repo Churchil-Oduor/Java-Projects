385b0b105b58d6f208ba0b59bb05d0ac3200deef
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-02-20 14:13:41 +0300

    LinkedList with add and View Functionality

---
+++ Data_Structures/Singly_LinkedList/LinkedList.java
@@ -0,0 +1,77 @@
+
+ public class LinkedList{
+
+     /*
+     * Instatiating the head node through a constructor
+     */
+    private Node head;
+    
+     LinkedList(){
+        head = new Node("Head Node");
+     }
+
+
+    private class Node{
+
+        private Node next ;
+        private String nodeName;
+
+       public Node(String nodeName){
+        next =null;
+        this.nodeName = nodeName;
+
+       }
+
+       public Node getNext(){
+            return next;
+       }
+
+       public String getNodeName(){
+        return nodeName;
+       }
+
+       public void setNext(Node next){
+        this.next = next;
+       }
+
+    }
+
+
+   
+     /*
+      * Method to add a new Node
+      */
+     
+      public void add(String nodeName){
+        
+        Node newNode = new Node(nodeName);
+        Node currentNode = head;
+
+        while(currentNode.getNext() != null){
+            currentNode = currentNode.getNext(); 
+        }
+
+        currentNode.setNext(newNode);
+      }
+
+      public void peekListData(){
+        Node headNode = head;
+
+        if(headNode.getNext() != null) {
+            do{
+                headNode = headNode.getNext();
+                System.out.println(headNode.getNodeName());
+            }
+            while( headNode.getNext() != null );
+        }
+
+        else{
+            System.out.println("Error -> You have not created any node");
+
+        }
+      }
+  
+
+}
+
+
